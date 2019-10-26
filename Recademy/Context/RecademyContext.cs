f5c0ff74710268ae820ba5ffdc8f918f2fccd50f
Riijii <Riijii333@gmail.com>
2019-10-26 19:28:55 +0300

    fix db context

--- Recademy/Context/RecademyContext.cs
+++ Recademy/Context/RecademyContext.cs
@@ -14,10 +14,25 @@ namespace Recademy.Context
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserSkill>()
+                .HasKey(bc => new { bc.UserId, bc.SkillName });
+
+            modelBuilder.Entity<ProjectSkill>()
+                .HasKey(bc => new { bc.ProjectId, bc.SkillName });
+
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Skill> Skills { get; set; }
         public DbSet<ReviewResponse> ReviewResponses { get; set; }
         public DbSet<ReviewRequest> ReviewRequests { get; set; }
         public DbSet<ProjectInfo> ProjectInfos { get; set; }
+
+        public DbSet<UserSkill> UserSkills { get; set; }
+
+        public DbSet<ProjectSkill> ProjectSkills { get; set; }
+
     }
 }
