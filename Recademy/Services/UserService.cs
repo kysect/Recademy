797dd57fd90a907907b16b4879ed528c66aadb82
Riijii <Riijii333@gmail.com>
2019-10-27 02:08:38 +0300

    UserInfoDto + some minor
    deleted achievments (replaced to achievementsDto)

--- Recademy/Services/UserService.cs
+++ Recademy/Services/UserService.cs
@@ -30,6 +30,41 @@ namespace Recademy.Services
             return userInfo;
         }
 
+        public UserInfoDto GetUserInfoDto(int userId)
+        {
+            User userInfo = Context.Users
+                .Include(s => s.ProjectInfos)
+                .Include(s => s.UserSkills)
+                .Include(u => u.ReviewRequests)
+                .FirstOrDefault(s => s.Id == userId);
+
+            List<string> skillNames = new List<string>();
+
+            AchievementService achievements = new AchievementService(Context);
+
+            foreach (var el in userInfo.UserSkills)
+            {
+                skillNames.Add(el.SkillName);
+            }
+
+            List<ProjectDto> projectInfos = new List<ProjectDto>();
+
+            foreach (var el in userInfo.ProjectInfos)
+            {
+                projectInfos.Add(ProjectDto.Of(el));
+            }
+
+            UserInfoDto result = new UserInfoDto()
+            {
+                UserName = userInfo.Name,
+                Activities = GetActivity(userId),
+                Skills = skillNames,
+                Achievements = achievements.GetAchievements(userId),
+                ProjectDtos = projectInfos
+            };
+
+            return result;
+        }
         /// <summary>
         /// return a user activity, index is month, value is activity number
         /// </summary>
@@ -66,17 +101,17 @@ namespace Recademy.Services
             int count = 0;
 
             var reviewResponses = Context.ReviewResponses.Where(x => x.ReviewerId == userId).ToList();
-            
+
             int year = DateTime.Now.Year;
             foreach (var el in reviewResponses)
             {
                 var reviewRequest = Context.ReviewRequests.Where(x => x.Id == el.ReviewRequestId).ToList().FirstOrDefault();
 
-                if(reviewRequest==null)
+                if (reviewRequest == null)
                     continue;
 
                 if (reviewRequest.DateCreate.Year == year)
-                   count++;
+                    count++;
             }
 
             return count;
@@ -86,7 +121,7 @@ namespace Recademy.Services
         /// key is user id, value is activity score
         /// </summary>
         /// <returns></returns>
-        public List<KeyValuePair<int,int>> GetRanking()
+        public List<KeyValuePair<int, int>> GetRanking()
         {
             List<KeyValuePair<int, int>> ranking = new List<KeyValuePair<int, int>>();
 
@@ -111,7 +146,7 @@ namespace Recademy.Services
                 AuthorId = argues.UserId,
                 GithubLink = argues.ProjectUrl,
                 Title = argues.ProjectName,
-                Skills = argues.Tags.Select(t => new ProjectSkill(){SkillName = t}).ToList()
+                Skills = argues.Tags.Select(t => new ProjectSkill() { SkillName = t }).ToList()
             };
 
             Context.ProjectInfos.Add(newProject);
