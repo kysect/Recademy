124bd7b2d7a31c0eaadbd6b9e83f4f2875615775
Riijii <Riijii333@gmail.com>
2019-10-26 23:45:04 +0300

    added ranking and fix activities

--- Recademy/Services/UserService.cs
+++ Recademy/Services/UserService.cs
@@ -35,16 +35,69 @@ namespace Recademy.Services
         /// <returns></returns>
         public List<int> GetActivity(int userId)
         {
-            List<int> result = Enumerable.Repeat(0,13).ToList();
+            List<int> result = Enumerable.Repeat(0, 13).ToList();
 
-            var reviewResponses = Context.ReviewResponses.Where(x => x.ReviewerId == userId);
+            var reviewResponses = Context.ReviewResponses.Where(x => x.ReviewerId == userId).ToList();
 
             int year = DateTime.Now.Year;
             foreach (var el in reviewResponses)
             {
-                if (el.ReviewRequest.DateCreate.Year == year)
+                var reviewRequest = Context.ReviewRequests.Where(x => x.Id == el.ReviewRequestId).ToList().FirstOrDefault();
+
+                if (reviewRequest == null)
+                    continue;
+
+                if (reviewRequest.DateCreate.Year == year)
                     result[el.ReviewRequest.DateCreate.Month]++;
             }
+
+            return result;
+        }
+
+        /// <summary>
+        /// get activity in count
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public int GetActivityInCount(int userId)
+        {
+            int count = 0;
+
+            var reviewResponses = Context.ReviewResponses.Where(x => x.ReviewerId == userId).ToList();
+            
+            int year = DateTime.Now.Year;
+            foreach (var el in reviewResponses)
+            {
+                var reviewRequest = Context.ReviewRequests.Where(x => x.Id == el.ReviewRequestId).ToList().FirstOrDefault();
+
+                if(reviewRequest==null)
+                    continue;
+
+                if (reviewRequest.DateCreate.Year == year)
+                   count++;
+            }
+
+            return count;
+        }
+        /// <summary>
+        /// get a score ranking by user's activities
+        /// key is user id, value is activity score
+        /// </summary>
+        /// <returns></returns>
+        public List<KeyValuePair<int,int>> GetRanking()
+        {
+            List<KeyValuePair<int, int>> ranking = new List<KeyValuePair<int, int>>();
+
+            var users = Context.Users.ToList();
+
+            foreach (var user in users)
+            {
+                var value = GetActivityInCount(user.Id);
+                if (value != 0)
+                    ranking.Add(new KeyValuePair<int, int>(user.Id, value));
+            }
+
+            var result = ranking.OrderByDescending(x => x.Value).ToList();
 
             return result;
         }
