5c7bb99fde8a6e7e62c0327cd75dd5d788a94027
Vlad <hrrrrustic@gmail.com>
2019-10-27 06:15:23 +0300

    Add some data to reviewPage

---
+++ Recademy/Dto/ReviewProjectDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Recademy.Dto
+{
+    public class ReviewProjectDto
+    {
+        public ReviewProjectDto(DateTime requesTime, string description, string title, string link)
+        {
+            RequesTime = requesTime;
+            Description = description;
+            Title = title;
+            Link = link;
+        }
+
+        public DateTime RequesTime { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+}
