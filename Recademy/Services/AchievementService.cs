62df86bcde7a6b5255259870ca328f2107f5c633
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-10-27 04:05:26 +0300

    feat: user service optimization

--- Recademy/Services/AchievementService.cs
+++ Recademy/Services/AchievementService.cs
@@ -1,45 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Recademy.Context;
+﻿using System.Collections.Generic;
 using Recademy.Dto;
+using Recademy.Models;
 using Recademy.Services.Abstraction;
 
 namespace Recademy.Services
 {
     public class AchievementService : IAchievementService
     {
-        public RecademyContext Context;
-
-        public AchievementService(RecademyContext context)
-        {
-            Context = context;
-        }
-
-        public List<AchievementsDto> GetAchievements(int userId)
+        public List<AchievementsDto> GetAchievements(User userInfo)
         {
             List<AchievementsDto> achievements = new List<AchievementsDto>();
-            UserService userService = new UserService(Context);
-            var user = userService.GetUserInfo(userId);
 
-            if (user.ReviewRequests.Count >= 1)
+            if (userInfo.ReviewRequests.Count >= 1)
             {
                 achievements.Add(new AchievementsDto() { Name = "First Request!", Description = "You did your first request, and we gave u some goods :)" });
             }
 
-            if (user.ProjectInfos.Count >= 3)
+            if (userInfo.ProjectInfos.Count >= 3)
             {
                 achievements.Add(new AchievementsDto()
                 { Name = "3 projects", Description = "You have at least 3 projects!" });
             }
 
 
-            if (user.UserSkills.Count >= 3)
+            if (userInfo.UserSkills.Count >= 3)
             {
                 achievements.Add(new AchievementsDto() { Name = "So experienced", Description = "You have skilled at least in 3 techologies, good job" });
             }
-            else if (user.UserSkills.Count >= 1)
+            else if (userInfo.UserSkills.Count >= 1)
             {
                 achievements.Add(new AchievementsDto() { Name = "You skilled!", Description = "You have skilled at least in 1 technology, u so good!" });
             }
