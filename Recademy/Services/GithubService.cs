b49d5e0c0c7dd0d702ce4e0eca4f42c0a8700561
Vlad <hrrrrustic@gmail.com>
2019-12-30 17:04:52 +0300

    refact github

--- Recademy/Services/GithubService.cs
+++ Recademy/Services/GithubService.cs
@@ -5,67 +5,58 @@ using Recademy.Dto;
 using Recademy.Utils;
 using Octokit;
 using Recademy.Services.Abstraction;
-using ProductHeaderValue = Octokit.ProductHeaderValue;
-using System;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Recademy.Services
 {
     public class GithubService : IGithubService
     {
+        private readonly GitHubClient _client = new GitHubClient(new ProductHeaderValue("Recademy"))
+        {
+            Credentials = new Credentials(GhUtil.Token)
+        };
+
         public List<GhRepositoryDto> GhGetRepositories(int userId)
         {
-            string accessToken = GhUtil.Token;
-            string clientId = GhUtil.ClientId;
-            string clientSecret = GhUtil.ClientSecret;
-
-            GitHubClient client = new GitHubClient(new ProductHeaderValue("Recademy"))
-            {
-                Credentials = new Credentials(accessToken)
-            };
-
-            IEnumerable<Repository> repositories = client
+            return _client
                 .Repository
                 .GetAllForCurrent()
                 .Result
-                .Where(k => !k.Private);
-
-            List<GhRepositoryDto> repoList = new List<GhRepositoryDto>();
-            foreach (Repository repository in repositories)
-            {
-                string readme;
-                try
+                .Where(k => !k.Private)
+                .Select(k => new GhRepositoryDto
                 {
-                    Readme request = client.Repository.Content.GetReadme(repository.Owner.Login, repository.Name).Result;
-                    readme = request.Content;
-                }
-                catch (AggregateException)
-                {
-                    readme = "No readme";
-                }
-
-                repoList.Add(new GhRepositoryDto
-                {
-                    RepositoryName = repository.Name,
-                    RepositoryUrl = repository.Url,
-                    Readme = readme,
-                    Language = repository.Language
-                });
-            }
-
-            return repoList;
+                    RepositoryName = k.Name,
+                    RepositoryUrl = k.Url,
+                    Readme = GetReadme(k),
+                    Language = k.Language
+                })
+                .ToList();
         }
 
+        public string GetReadme(Repository repository)
+        {
+            return GetReadme(repository.Owner.Login, repository.Name);
+        }
+
+        private string GetReadme(string login, string repositoryName)
+        {
+            try
+            {
+                return _client
+                    .Repository
+                    .Content
+                    .GetReadme(login, repositoryName)
+                    .Result
+                    .Content;
+            }
+            catch (AggregateException)
+            {
+                return "No readme";
+            }
+        }
         public async Task CreateIssues(string repoLink, string issueText)
         {
             repoLink = repoLink.Replace("/repos/", "/");
-            string accessToken = GhUtil.Token;
-            GitHubClient client = new GitHubClient(new ProductHeaderValue("Recademy"))
-            {
-                Credentials = new Credentials(accessToken)
-            };
-
             string[] splittedUrl = repoLink.Split('/');
             string issueName = GhUtil.IssueText + "Test Reviewer";
             NewIssue issue = new NewIssue(issueName)
@@ -73,35 +64,15 @@ namespace Recademy.Services
                 Body = issueText
             };
 
-            await client
+            await _client
                 .Issue
                 .Create(splittedUrl[3], splittedUrl[4], issue);
         }
 
-        public static string GetReadme(string repoLink)
+        public string GetReadme(string repoLink)
         {
-            string accessToken = GhUtil.Token;
-            string clientId = GhUtil.ClientId;
-            string clientSecret = GhUtil.ClientSecret;
-
-            GitHubClient client = new GitHubClient(new ProductHeaderValue("Recademy"))
-            {
-                Credentials = new Credentials(accessToken)
-            };
-
             string[] splittedUrl = repoLink.Split('/');
-            string readme;
-            try
-            {
-                Readme request = client.Repository.Content.GetReadme(splittedUrl[3], splittedUrl[4]).Result;
-                readme = request.Content;
-            }
-            catch (AggregateException)
-            {
-                readme = "No readme";
-            }
-
-            return readme;
+            return GetReadme(splittedUrl[3], splittedUrl[4]);
         }
     }
 }
