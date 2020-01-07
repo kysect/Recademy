74a44ccd25a76e78eb963c2bad21894d524664d7
s4xack <sergmiron47@gmail.com>
2020-01-08 00:43:06 +0300

    fix: types and tokens

--- Recademy/Utils/GhUtil.cs
+++ Recademy/Utils/GhUtil.cs
@@ -2,7 +2,7 @@
 {
     public class GhUtil
     {
-        public static string Token = "33e08f23990afb309ef6c4ebac774785cc92e26c";
+        public static string Token = "place here your token";
         public static string ClientId = "e52a5dcb47bfa2367cce";
         public static string ClientSecret = "33ae75d4e83a1d192529fb642a6abd58e8ed1884";
         public static string IssueText = "Recademy : review by ";
