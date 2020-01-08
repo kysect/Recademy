b04088750f45e1095343d52b2ed01fe2c8f53a7c
Riijii <Riijii333@gmail.com>
2020-01-08 04:48:35 +0400

    mock update
    removed useless staff

--- Mock/Generators/Utilities.cs
+++ Mock/Generators/Utilities.cs
@@ -4,8 +4,6 @@ namespace Mock.Generators
 {
     public static class Utilities
     {
-        public static int CurrentUserId { get; set; }
-
         public static Random Random { get; set; } = new Random();
     }
 }
