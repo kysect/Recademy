bf4d0081fd513df761424ec897ae474bdf3a6e8c
Riijii <Riijii333@gmail.com>
2020-01-08 16:19:03 +0400

    sensetive information removed

--- Recademy/Utils/GhUtil.cs
+++ Recademy/Utils/GhUtil.cs
@@ -3,8 +3,6 @@
     public class GhUtil
     {
         public static string Token = "place here your token";
-        public static string ClientId = "e52a5dcb47bfa2367cce";
-        public static string ClientSecret = "33ae75d4e83a1d192529fb642a6abd58e8ed1884";
         public static string IssueText = "Recademy : review by ";
     }
 }
