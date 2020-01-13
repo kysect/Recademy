0267c8e81309ee5a8695608423ddb2ca9010f750
Vlad <hrrrrustic@gmail.com>
2020-01-13 07:32:53 +0300

    add swagger

--- RecademyApi/Startup.cs
+++ RecademyApi/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace RecademyApi
 {
@@ -21,31 +24,30 @@ namespace RecademyApi
         }
 
         public IConfiguration Configuration { get; }
-
+            
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(configuration =>
+            {
+                configuration.SwaggerDoc("Recademy", new OpenApiInfo
+                {
+                    Title = "Recademy API",
+                    Version = "0.0.1"
+                });
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                configuration.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseSwagger();
+            app.UseSwaggerUI(configuration =>
+                configuration.SwaggerEndpoint("/swagger/Recademy/swagger.json", "Recademy API"));
         }
     }
 }
