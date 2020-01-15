8d866473cc73e17c98494deb10e1f695c20b4e2f
Vlad <hrrrrustic@gmail.com>
2020-01-15 12:49:30 +0300

    Convert ReviewPage to controllers

--- Recademy.Api/Controllers/ReviewController.cs
+++ Recademy.Api/Controllers/ReviewController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Recademy.Api.Services.Abstraction;
+using Recademy.Library.Dto;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,6 +26,30 @@ namespace Recademy.Api.Controllers
 
             _reviewService.AddReviewRequest(projectId);
             return Accepted();
+        }
+
+        [HttpGet]
+        [Route("{requestId}")]
+        public IActionResult GetReviewRequestInfo([FromQuery] int requestId)
+        {
+            if (requestId < 0)
+                return BadRequest("Wrong request id");
+
+            ReviewProjectDto reviewInfo = _reviewService.GetReviewInfo(requestId);
+            return Ok(reviewInfo);
+        }
+        [HttpPost]
+        [Route("{requestId}")]
+        public IActionResult CreateReviewResponse([FromQuery] int requestId, [FromBody] string reviewText)
+        {
+            if (requestId < 0)
+                return BadRequest("Wrong request id");
+
+            if (string.IsNullOrWhiteSpace(reviewText))
+                return BadRequest("Wrong issue");
+
+            _reviewService.SendReviewResponse(new SendReviewRequestDto(requestId, reviewText));
+            return Ok();
         }
     }
 }
