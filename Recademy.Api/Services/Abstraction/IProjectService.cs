086ae11ec57d25fb60add922952fc1c19b8caf3c
Vlad <hrrrrustic@gmail.com>
2020-01-17 20:40:52 +0300

    move GetProjectsByTag to project service, fix interfaces

--- Recademy.Api/Services/Abstraction/IProjectService.cs
+++ Recademy.Api/Services/Abstraction/IProjectService.cs
@@ -1,9 +1,12 @@
-﻿using Recademy.Library.Models;
+﻿using System.Collections.Generic;
+using Recademy.Library.Dto;
+using Recademy.Library.Models;
 
 namespace Recademy.Api.Services.Abstraction
 {
     public interface IProjectService
     {
         ProjectInfo GetProjectInfo(int projectId);
+        List<ProjectDto> GetProjectsByTag(string tagName);
     }
 }
