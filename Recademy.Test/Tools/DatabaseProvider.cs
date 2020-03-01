225337900abc5705140e1e95ac1e7133f21ce53d
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-03-02 02:46:22 +0300

    feat: add database context for test creator

---
+++ Recademy.Test/Tools/DatabaseProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Recademy.Api;
+
+namespace Recademy.Test.Tools
+{
+    public static class DatabaseProvider
+    {
+        private static RecademyContext GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<RecademyContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new RecademyContext(options);
+            databaseContext.Database.EnsureCreated();
+
+            //TODO: use mocker
+
+            return databaseContext;
+        }
+    }
+}
