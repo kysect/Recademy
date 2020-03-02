758647d757cc6845cc1c2e03e8c7652751949036
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-03-02 12:32:50 +0300

    refactor: change test from services to controllers

--- Recademy.Test/Controllers/UserControllerTests.cs
+++ Recademy.Test/Controllers/UserControllerTests.cs
@@ -1,18 +1,21 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 using Recademy.Api;
+using Recademy.Api.Controllers;
 using Recademy.Api.Services;
 using Recademy.Library.Dto;
 using Recademy.Library.Models;
 using Recademy.Mock;
 using Recademy.Test.Tools;
 
-namespace Recademy.Test.Services
+namespace Recademy.Test.Controllers
 {
-    public class UserServiceTests
+    public class UserControllerTests
     {
         private RecademyContext _context;
         private Mocker _mocker;
-        private UserService _userService; 
+        private UserService _userService;
+        private UserController _userController;
 
         [SetUp]
         public void Setup()
@@ -20,6 +23,7 @@ namespace Recademy.Test.Services
             _context = TestDatabaseProvider.GetDatabaseContext();
             _mocker = new Mocker(_context);
             _userService = new UserService(_context, new AchievementService());
+            _userController = new UserController(_userService);
         }
 
         [Test]
@@ -27,9 +31,9 @@ namespace Recademy.Test.Services
         {
             User generatedUser = _mocker.GenerateUser();
 
-            UserInfoDto userFromDb = _userService.GetUserInfo(generatedUser.Id);
+            ActionResult<UserInfoDto> result = _userController.GetUserInfo(generatedUser.Id);
 
-            Assert.NotNull(userFromDb);
+            Assert.NotNull(result.Value);
         }
     }
 }
