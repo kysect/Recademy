34c9bc9fb0843ab82fa670332e3df4fcde457c2e
Fredi Kats <fredikats@outlook.com>
2020-03-02 21:55:03 +0300

    test: fail on creating second review for project

--- Recademy.Test/Controllers/ReviewControllerTests.cs
+++ Recademy.Test/Controllers/ReviewControllerTests.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Recademy.Api;
 using Recademy.Api.Controllers;
 using Recademy.Api.Services;
 using Recademy.Library.Dto;
 using Recademy.Library.Models;
+using Recademy.Library.Types;
 using Recademy.Mock;
 using Recademy.Mock.Generators;
 using Recademy.Test.Tools;
@@ -15,7 +15,6 @@ namespace Recademy.Test.Controllers
     {
         private RecademyContext _context;
         private Mocker _mocker;
-        private UserController _userController;
         private ProjectController _projectController;
         private ReviewController _reviewController;
 
@@ -24,13 +23,12 @@ namespace Recademy.Test.Controllers
         {
             _context = TestDatabaseProvider.GetDatabaseContext();
             _mocker = new Mocker(_context);
-            _userController = new UserController(new UserService(_context, new AchievementService()));
             _projectController = new ProjectController(new ProjectService(_context));
             _reviewController = new ReviewController(new ReviewService(_context));
         }
 
         [Test]
-        public void AddProject_UserHasProject()
+        public void AddProjectReview_ShouldExist()
         {
             User studentAccount = _mocker.GenerateUser();
             User mentorAccount = _mocker.GenerateUser();
@@ -43,6 +41,21 @@ namespace Recademy.Test.Controllers
             ReviewRequestInfoDto review = _reviewController.CreateReviewRequest(reviewRequestAddDto).Value;
 
             Assert.NotNull(review);
+        }
+
+        [Test]
+        public void AddTwoSameProjectReview_ShouldFailWithException()
+        {
+            User studentAccount = _mocker.GenerateUser();
+            User mentorAccount = _mocker.GenerateUser();
+            AddProjectDto addProjectDto = InstanceFactory.CreateAddProjectDto(studentAccount.Id);
+
+            ProjectInfoDto createdProject = _projectController.AddUserProject(addProjectDto).Value;
+            ReviewRequestAddDto reviewRequestAddDto =
+                InstanceFactory.CreateReviewRequestAddDto(mentorAccount.Id, createdProject.ProjectId);
+            ReviewRequestInfoDto review = _reviewController.CreateReviewRequest(reviewRequestAddDto).Value;
+            
+            Assert.Catch<RecademyException>(() => _reviewController.CreateReviewRequest(reviewRequestAddDto));
         }
     }
 }
