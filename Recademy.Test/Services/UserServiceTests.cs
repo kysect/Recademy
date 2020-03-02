6bf471f7dab618315dfa760d6b45180ad82e6298
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-03-02 11:40:31 +0300

    test: add test for user service

---
+++ Recademy.Test/Services/UserServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Recademy.Api;
+using Recademy.Api.Services;
+using Recademy.Library.Dto;
+using Recademy.Library.Models;
+using Recademy.Mock;
+using Recademy.Test.Tools;
+
+namespace Recademy.Test.Services
+{
+    public class UserServiceTests
+    {
+        private RecademyContext _context;
+        private Mocker _mocker;
+        private UserService _userService; 
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = TestDatabaseProvider.GetDatabaseContext();
+            _mocker = new Mocker(_context);
+            _userService = new UserService(_context, new AchievementService());
+        }
+
+        [Test]
+        public void MockDatabase_AtLeastOneUserExist()
+        {
+            User generatedUser = _mocker.GenerateUser();
+
+            UserInfoDto userFromDb = _userService.GetUserInfo(generatedUser.Id);
+
+            Assert.NotNull(userFromDb);
+        }
+    }
+}
