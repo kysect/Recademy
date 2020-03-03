2081bc047be81f0fcd8706039402d18ebc736a1c
Fredi Kats <fredikats@outlook.com>
2020-03-03 23:19:13 +0300

    refactor: rewrite test with test case context

--- Recademy.Test/Controllers/GamificationControllerTest.cs
+++ Recademy.Test/Controllers/GamificationControllerTest.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using Recademy.Api;
 using Recademy.Api.Controllers;
 using Recademy.Api.Services;
-using Recademy.Mock;
 using Recademy.Test.Tools;
 
 namespace Recademy.Test.Controllers
@@ -11,18 +10,12 @@ namespace Recademy.Test.Controllers
     public class GamificationControllerTest
     {
         private RecademyContext _context;
-        private Mocker _mocker;
-        private UserController _userController;
-        private ProjectController _projectController;
         private GamificationController _gamificationController;
 
         [SetUp]
         public void Setup()
         {
             _context = TestDatabaseProvider.GetDatabaseContext();
-            _mocker = new Mocker(_context);
-            _userController = new UserController(new UserService(_context, new AchievementService()));
-            _projectController = new ProjectController(new ProjectService(_context));
             _gamificationController = new GamificationController(new GamificationService(_context));
         }
 
