cb8a0731f16e3410890d851a6b3482a50e375ea5
riijii <riijii333@gmail.com>
2020-11-03 18:27:35 +0300

    feat: authorization with registration

--- Recademy.Api/Controllers/AuthController.cs
+++ Recademy.Api/Controllers/AuthController.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Recademy.Api.Services.Abstraction;
 
 namespace Recademy.Api.Controllers
 {
@@ -6,5 +15,39 @@ namespace Recademy.Api.Controllers
     [ApiController]
     public class AuthController : Controller
     {
+        private readonly IOauthProviderService _oauthProvider;
+        private readonly IRegisterService _registerService;
+
+        public AuthController(IOauthProviderService oauthProvider, IRegisterService registerService)
+        {
+            _oauthProvider = oauthProvider;
+            _registerService = registerService;
+        }
+
+        [HttpGet("signIn")]
+        public IActionResult SignIn(string provider)
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = "/api/auth/register" }, provider);
+        }
+
+        [HttpGet("signOut")]
+        public IActionResult SignOut()
+        {
+            return SignOut(new AuthenticationProperties() { RedirectUri = "/" },
+                CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        [Authorize]
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            var user = _registerService.GetUserFromClaims(HttpContext.User);
+            
+            if (user == null)
+                return BadRequest();
+
+            _registerService.Register(user);
+            return Redirect("/");
+        }
     }
 }
---
+++ Recademy.Api/Services/Abstraction/IRegisterService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recademy.Library.Models;
+
+namespace Recademy.Api.Services.Abstraction
+{
+    public interface IRegisterService
+    {
+        public void Register(User user);
+        public User GetUserFromClaims(ClaimsPrincipal claims);
+        public bool IsUserRegistered(User user);
+    }
+}
