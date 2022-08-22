d3deaa39cd369e5f0e9c0435481d32fe4842afde
annchous <annchooousss@gmail.com>
2022-08-22 14:37:31 +0400

    fix: replace const with static

--- Recademy.Api/GhUtil.cs
+++ Recademy.Api/GhUtil.cs
@@ -1,10 +1,10 @@
-﻿namespace Recademy.Api
+namespace Recademy.Api
 {
     public static class GhUtil
     {
         public const string AppClientId = "place client id here";
         public const string AppSecret = "place app secret here";
-        public const string Token = "place here your token";
+        public static string Token = "place here your token";
         public const string IssueText = "[Recademy review]";
     }
 }
