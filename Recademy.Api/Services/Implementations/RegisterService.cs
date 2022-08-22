d2ae6959f68f67cc11145c30c611ca51bea2cd68
annchous <annchooousss@gmail.com>
2022-08-22 14:23:49 +0400

    feat: add argument null check

--- Recademy.Api/Services/Implementations/RegisterService.cs
+++ Recademy.Api/Services/Implementations/RegisterService.cs
@@ -19,6 +19,8 @@ namespace Recademy.Api.Services.Implementations
 
         public void Register(User user)
         {
+            ArgumentNullException.ThrowIfNull(user);
+
             if (IsUserRegistered(user))
                 return;
 
