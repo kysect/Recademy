bf1b004a96e4e016151ceb86bd17cadc758d2f31
annchous <annchooousss@gmail.com>
2022-08-23 23:45:59 +0400

    feat: add user achievement interface

---
+++ Recademy.Core/Models/Achievements/IUserAchievement.cs
@@ -0,0 +1,9 @@
+﻿namespace Recademy.Core.Models.Achievements;
+
+public interface IUserAchievement
+{
+    public int Id { get; }
+    public string Title { get; }
+    public string Description { get; }
+    public int Points { get; }
+}
