eac6adc72b51e58b05427d84cb52c47dd3ddb3dc
annchous <annchooousss@gmail.com>
2022-08-23 23:46:44 +0400

    feat: add achievements service

---
+++ Recademy.Api/Services/Abstraction/IUserAchievementService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Recademy.Core.Models.Achievements;
+
+namespace Recademy.Api.Services.Abstraction;
+
+public interface IUserAchievementService
+{
+    IReadOnlyCollection<IUserAchievement> GetAllAchievements();
+}
---
+++ Recademy.Api/Services/Implementations/UserAchievementService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Recademy.Api.Services.Abstraction;
+using Recademy.Core.Models.Achievements;
+
+namespace Recademy.Core;
+
+public sealed class UserAchievementService : IUserAchievementService
+{
+    private readonly IReadOnlyCollection<IUserAchievement> _achievements = new List<IUserAchievement>()
+    {
+        new FirstTimeUserAchievement(),
+        new NeatUserAchievement(),
+    };
+
+    public IReadOnlyCollection<IUserAchievement> GetAllAchievements()
+    {
+        return _achievements;
+    }
+}
