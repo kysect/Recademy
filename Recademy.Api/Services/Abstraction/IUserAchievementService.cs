e01a71a8dd929e873332475e3345b014aaa37f1f
annchous <annchooousss@gmail.com>
2022-08-24 13:18:20 +0400

    feat: add user achievement model

--- Recademy.Api/Services/Abstraction/IUserAchievementService.cs
+++ Recademy.Api/Services/Abstraction/IUserAchievementService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
 using Recademy.Core.Models.Achievements;
 
 namespace Recademy.Api.Services.Abstraction;
@@ -6,4 +8,7 @@ namespace Recademy.Api.Services.Abstraction;
 public interface IUserAchievementService
 {
     IReadOnlyCollection<IUserAchievement> GetAllAchievements();
+    IReadOnlyCollection<IUserAchievement> GetUserAchievements(int userId);
+    IReadOnlyCollection<IUserAchievement> GetUserAchievements(string username);
+    Task AddUserAchievement(int userId, int achievementId);
 }
