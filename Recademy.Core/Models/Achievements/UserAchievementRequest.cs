de6495a574bb5807850f0398e6a3fe6a067c085c
annchous <annchooousss@gmail.com>
2022-08-31 00:31:02 +0400

    feat: add achievement request model

---
+++ Recademy.Core/Models/Achievements/UserAchievementRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Recademy.Core.Models.Users;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recademy.Core.Models.Achievements;
+
+public class UserAchievementRequest
+{
+    [Key]
+    public int RequestId { get; set; }
+    [ForeignKey("RecademyUser")]
+    public int UserId { get; set; }
+    public RecademyUser User { get; set; }
+    public int AchievementId { get; set; }
+    public string Reason { get; set; }
+    public DateTime RequestTime { get; set; }
+}
