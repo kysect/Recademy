c19796438e57220bec89fb943f0da554235809ad
annchous <annchooousss@gmail.com>
2022-08-30 21:31:14 +0400

    feat: add user role dto

---
+++ Recademy.Shared/Roles/UserRoleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Recademy.Dto.Roles;
+
+public sealed class UserRoleDto
+{
+    public UserRoleDto()
+    {
+    }
+
+    public string Name { get; init; }
+    public int RequiredPoints { get; init; }
+}
