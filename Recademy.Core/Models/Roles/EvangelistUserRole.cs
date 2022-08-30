8eaa060b922e8935f21bdc60af649e43efb9822b
annchous <annchooousss@gmail.com>
2022-08-30 21:30:42 +0400

    feat: add evangelist role

---
+++ Recademy.Core/Models/Roles/EvangelistUserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace Recademy.Core.Models.Roles;
+
+public sealed class EvangelistUserRole : IUserRole
+{
+    public int Id => 1;
+    public string Name => "Евангелист";
+    public int RequiredPoints => 1000;
+}
