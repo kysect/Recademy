5fc160aa5ed192d1efd5805725fe04dbd4f510a6
annchous <annchooousss@gmail.com>
2022-08-30 21:31:31 +0400

    feat: add user role service and mapping

---
+++ Recademy.Application/Mappings/RoleMappingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Recademy.Core.Models.Roles;
+using Recademy.Dto.Roles;
+
+namespace Recademy.Application.Mappings;
+
+public static class RoleMappingExtensions
+{
+    public static UserRoleDto ToDto(this IUserRole role)
+    {
+        if (role is null)
+            return null;
+
+        return new UserRoleDto
+        {
+            Name = role.Name,
+            RequiredPoints = role.RequiredPoints,
+        };
+    }
+}
---
+++ Recademy.Application/Services/Abstractions/IUserRoleService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Recademy.Dto.Roles;
+
+namespace Recademy.Application.Services.Abstractions;
+
+public interface IUserRoleService
+{
+    IReadOnlyCollection<UserRoleDto> GetAllRoles();
+    UserRoleDto GetUserRole(int userId);
+    Task ChangeUserRole(int userId, int roleId);
+}
