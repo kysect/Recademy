dad6b293ce8938bb74c926c1c706984cbd388a61
ALEXXXANDRO <alkolegov2011@yandex.ru>
2022-12-18 18:48:52 +0300

    feat: added service interface

---
+++ Recademy.Application/Services/Abstractions/IUserActivityService.cs
@@ -0,0 +1,12 @@
+﻿using Recademy.Dto.Activity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Recademy.Application.Services.Abstractions;
+
+public interface IUserActivityService
+{
+    public Task<IReadOnlyCollection<UserActivityDto>> GetAllActivity();
+    public Task<UserActivityDto> GetUserActivity(string userName);
+}
