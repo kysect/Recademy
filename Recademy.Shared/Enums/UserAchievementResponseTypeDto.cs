88aba2ea30993b604b433b8a9833dbfdeb2ecf91
annchous <annchooousss@gmail.com>
2022-12-18 14:50:19 +0400

    refactor: translate values to russian

--- Recademy.Shared/Enums/UserAchievementResponseTypeDto.cs
+++ Recademy.Shared/Enums/UserAchievementResponseTypeDto.cs
@@ -1,8 +1,24 @@
-﻿namespace Recademy.Dto.Enums;
+﻿using System;
+
+namespace Recademy.Dto.Enums;
 
 public enum UserAchievementResponseTypeDto
 {
     Approved,
     Declined,
     NoResponse,
+}
+
+public static class UserAchievementResponseTypeExtensions
+{
+    public static string TranslateToString(this UserAchievementResponseTypeDto response)
+    {
+        return response switch
+        {
+            UserAchievementResponseTypeDto.Approved => "Одобрено",
+            UserAchievementResponseTypeDto.Declined => "Отклонено",
+            UserAchievementResponseTypeDto.NoResponse => "Без ответа",
+            _ => throw new ArgumentOutOfRangeException(nameof(response), response, null)
+        };
+    }
 }
