003fd9b909f5f39bcb02bb12c7cac2f8a64fd9b6
annchous <annchooousss@gmail.com>
2022-12-18 15:01:36 +0400

    refactor: rework review requests displaying

--- Recademy.Shared/Enums/ProjectStateDto.cs
+++ Recademy.Shared/Enums/ProjectStateDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Recademy.Dto.Enums;
+﻿using System;
+
+namespace Recademy.Dto.Enums;
 
 public enum ProjectStateDto
 {
@@ -6,4 +8,19 @@ public enum ProjectStateDto
     Reviewed = 2,
     Completed = 3,
     Abandoned = 4,
+}
+
+public static class ProjectStateExtensions
+{
+    public static string TranslateToString(this ProjectStateDto state)
+    {
+        return state switch
+        {
+            ProjectStateDto.Requested => "Запрошено ревью",
+            ProjectStateDto.Reviewed => "Получено ревью",
+            ProjectStateDto.Completed => "Завершено",
+            ProjectStateDto.Abandoned => "Отклонено",
+            _ => throw new ArgumentOutOfRangeException(nameof(state), state, null)
+        };
+    }
 }
