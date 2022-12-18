c23fe3dabe26bcb598b6dcbbd48aa78295c6fe39
ALEXXXANDRO <alkolegov2011@yandex.ru>
2022-12-18 18:49:18 +0300

    feat: added UserActivityDto

---
+++ Recademy.Application/Mappings/UserActivityMappingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Kysect.GithubUtils.OrganizationContributions;
+using Recademy.Dto.Activity;
+
+namespace Recademy.Application.Mappings;
+
+public static class ActivityMappingExtensions
+{
+    public static UserActivityDto ToDto(this OrganizationContributor organizationContributor)
+    {
+        if (organizationContributor is null)
+            return null;
+
+        return new UserActivityDto()
+        {
+            Name = organizationContributor.Username,
+            ContributionsCount = organizationContributor.Contributions
+        };
+    }
+}
---
+++ Recademy.Shared/Activity/UserActivityDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Recademy.Dto.Activity;
+
+public sealed class UserActivityDto
+{
+    public string Name { get; init; }
+    public int ContributionsCount { get; init; }
+}
