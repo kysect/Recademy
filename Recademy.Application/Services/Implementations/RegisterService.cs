327f75f5e9d619a5b5f3d3a0639057ce33f30202
annchous <annchooousss@gmail.com>
2022-12-19 17:52:23 +0400

    fix: user saving

--- Recademy.Application/Services/Implementations/RegisterService.cs
+++ Recademy.Application/Services/Implementations/RegisterService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Recademy.Application.Services.Abstractions;
 using Recademy.Core.Models.Users;
 using Recademy.DataAccess;
@@ -12,11 +13,13 @@ public class RegisterService : IRegisterService
 {
     private readonly RecademyContext _context;
     private readonly IOauthProviderService _oauthProvider;
+    private readonly ILogger<RegisterService> _logger;
 
-    public RegisterService(RecademyContext context, IOauthProviderService oauthProvider)
+    public RegisterService(RecademyContext context, IOauthProviderService oauthProvider, ILogger<RegisterService> logger)
     {
         _context = context;
         _oauthProvider = oauthProvider;
+        _logger = logger;
     }
 
     public void Register(User user)
@@ -27,9 +30,16 @@ public class RegisterService : IRegisterService
             return;
 
         _context.Users.Add(user);
-        _context.RecademyUsers.Add(new RecademyUser { UserId = user.Id });
-
         _context.SaveChanges();
+
+        _logger.LogInformation($"Registered user {user.GithubUsername} with ID {user.Id}");
+
+        var recademyUser = new RecademyUser { UserId = user.Id };
+
+        _context.RecademyUsers.Add(recademyUser);
+        _context.SaveChanges();
+
+        _logger.LogInformation($"Registered Recademy user {recademyUser.User.GithubUsername} with ID {recademyUser.UserId}");
     }
 
     public User GetUserFromClaims(ClaimsPrincipal claims)
